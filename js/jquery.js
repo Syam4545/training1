9988632cde75011fe9ee5d6c2532f4af85bd90f0
syam4545 <syamsubramanian45@gmail.com>
2023-10-03 09:51:58 +0530

    gitignore

--- js/jquery.js
+++ js/jquery.js
@@ -1,24 +1,42 @@
+
+//click to hide
 $(document).ready(function(){
 $(".p1").click (function(){
     $(this).hide();
 });
 
-
 $("#p2").click (function(){
     $(this).hide();
 });
-
-
-
-
-
 });
 
+//hide button and show button
 $(document).ready(function(){
     $("#hide").click(function(){
-      $("p").hide();
+      $(".p3").hide();
     });
     $("#show").click(function(){
-      $("p").show();
+      $(".p3").show();
     });
   });
+
+//double click to hide
+
+  $(document).ready(function(){
+    $(".p4").dblclick(function(){
+      $(this).hide();
+    });
+  });
+
+  //mouse hover to hide
+
+  $(document).ready(function(){
+    $(".p5").mouseenter(function(){
+      $(".p5").hide();
+  });
+  $("#show1").click(function(){
+    $(".p5").show();
+  });
+  });
+
+ 
